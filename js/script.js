da7433b5d406dfbfd628a9b292a54ba72930efc3
tahakyo <ghazouani1988@email.com>
2022-04-11 16:07:41 +0200

    add 'fizz' logic in js

---
+++ js/script.js
@@ -0,0 +1,28 @@
+// Output nella console:
+// 1
+// 2
+// Fizz 
+// 4
+// Buzz
+// Fizz
+// 7
+// 8
+// Fizz 
+
+// ..
+// 13
+// 14
+// FizzBuzz 
+// ..
+// ..
+
+// stampa dei numeri nella console
+for (let i = 1; i<= 100; ++i) {
+    console.log(i);
+    // definire se fizz o buzz
+    let fizzBuzz;
+    if (i % 3 === 0) {
+        fizzBuzz = "Fizz";
+        console.log(fizzBuzz);
+    }
+}
